64593f2998c6dd791ce1dc5deb475d973210078b
Uriel <uventura@simetrical.com.mx>
2019-07-03 14:44:20 -0500

    Directory, Controllers, Models and Migrations

---
+++ database/migrations/2019_07_03_194213_create_arbitros_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArbitrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arbitros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arbitros');
+    }
+}
